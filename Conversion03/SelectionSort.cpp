e83a4a744f127f4986d7ef7709b329fa28ad14af
angelc721 <86585097+angelc721@users.noreply.github.com>
2022-02-11 14:55:58 +0800

    upload exercise

--- Conversion03/SelectionSort.cpp
+++ Conversion03/SelectionSort.cpp
@@ -22,19 +22,18 @@ int main()
     }
     cout<<endl;
 
-    for(int i=0; i<size-1; i++)
+    for (int i=0; i<size-1; i++) 
     {
-        min_idx = i; 
-        for(int j=i+1; j<size; j++)
-        {
-            if(arr[j]<arr[min_idx])
+        int min_idx = i;
+        for (int j=i+1; j<size; j++) {
+            if (arr[j] < arr[min_idx]) 
             {
-                min_idx = j; 
-                int temp=arr[min_idx];
-                arr[min_idx]=arr[i];
-                arr[i]=temp; 
+                min_idx = j;
             }
         }
+        int temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
     }
     cout<<"after : ";
 		for ( int i=0; i<size; i++ )
